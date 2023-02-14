5fdea6c2778002e4c475c13e8c48b151493969c2
Hosealeonardo18 <hosealeonardo18@gmail.com>
2023-02-15 00:35:35 +0700

    add navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import style from './style.module.css';
+
+const Navbar = () => {
+  return (
+    <nav className={`navbar ${style.navbarContainer} navbar-expand-lg bg-body-tertiary`}>
+      <div className="container">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className={`nav-link ${style.navLink} active bg-warning`} href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
