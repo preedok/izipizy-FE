7bbe6e5479aefbced98eeff74cddb29cba00c5fa
Hello Dok <edo.vivoy21@gmail.com>
2023-02-15 16:51:29 +0700

    update conflig footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import style from "../Footer/style.module.css";
+import style from "../style/style.module.css";
 
 const Footer = () => {
   return (
